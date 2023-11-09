1ebce8ea19637168cb963795486b34cf2ff32bc6
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-09 22:57:25 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const elementUl = document.getElementById('ingredients');
+const fragment = document.createDocumentFragment();
+ingredients.forEach(el => { 
+  const liElement = document.createElement('li');
+  liElement.classList.add('item');
+  liElement.textContent = el;
+  fragment.appendChild(liElement);
+});
+elementUl.appendChild(fragment);
