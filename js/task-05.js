d8b9bf6eee8a020b048da8d4bbada3dfc8891647
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-15 22:52:17 +0200

    final task5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+nameInput.addEventListener('input', function (ev) {
+    if (nameInput.value === '') {
+        nameOutput.textContent = 'Anonymous';
+    } else {
+        nameOutput.textContent = nameInput.value;
+    }
+});
