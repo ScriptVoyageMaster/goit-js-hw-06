298563c6a759d467d43483cfe0e93b13d68150f1
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-19 23:06:58 +0200

    final task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    const inputs = Array.from(ev.target.elements).filter(el=>el.tagName === 'INPUT');
+    //console.log(inputs);
+    if (!checkingEmptyString(inputs)) {
+        const obgectForm = {};
+        inputs.forEach(el => {
+            obgectForm[el.type] = el.value;
+            el.value = "";
+        });  
+        console.log(obgectForm);
+    }
+});
+ function checkingEmptyString(inputs) {
+   for (const el of inputs) {
+         if ( el.value.trim() === "") { 
+            alert("Всі поля повинні бути заповнені");
+            return true;
+        } 
+    } 
+ }
