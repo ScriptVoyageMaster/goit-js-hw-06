cc4f8ede48b7b351e60d4c5533ad5a530947497d
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-20 00:34:07 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,8 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.getElementById("controls").children;
+console.log(controls[1]);
+controls.add
+function createBoxes(amount) { };
+function destroyBoxes() { };
