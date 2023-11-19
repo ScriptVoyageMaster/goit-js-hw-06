5c1136bd29b3caf2cff2565ed60a22621a0c355b
ScriptVoyageMaster <139281125+ScriptVoyageMaster@users.noreply.github.com>
2023-11-19 23:49:16 +0200

    final task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+
+function changeColor() {
+    const hehColor = getRandomHexColor();
+    color.textContent = hehColor;
+    document.body.style.backgroundColor = hehColor;
+}
+button.addEventListener('click', changeColor);
+//setInterval(changeColor,5000);
